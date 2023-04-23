9c3f5598c44107f22690f8f53844d4af43001372
MarieApparailly <mapparailly@ensc.fr>
2023-04-23 04:32:17 +0200

    Mix Project v1 + v2 ok : geolocation + affichage maps + navigation stack/tab/screen

--- screens/UsersMapScreen.js
+++ screens/UsersMapScreen.js
@@ -1,11 +1,137 @@
-import React from 'react';
-import {SafeAreaView, TouchableOpacity, StyleSheet, View} from 'react-native';
+// import {
+//   SafeAreaView,
+//   View,
+//   Text,
+//   StyleSheet,
+//   StatusBar,
+//   Image,
+//   Button,
+// } from 'react-native';
+
+// const RootTabNavigator = () => {
+
+//   //   return (
+//   //     <SafeAreaView style={{flex: 1}}>
+
+//   //     </SafeAreaView>
+//   //   );
+// };
+
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+  PermissionsAndroid,
+  Platform,
+  View,
+} from 'react-native';
+
+//import all the components we are going to use.
+import Geolocation from '@react-native-community/geolocation';
+
 import MapView, {Marker} from 'react-native-maps';
 import UsersListScreen from './UsersListScreen';
 
 const UsersMapScreen = ({navigation}) => {
   const list = () => {
     return <UsersListScreen />;
+  };
+
+  const [currentLongitude, setCurrentLongitude] = useState('...');
+  const [currentLatitude, setCurrentLatitude] = useState('...');
+  const [locationStatus, setLocationStatus] = useState('');
+
+  useEffect(() => {
+    const requestLocationPermission = async () => {
+      if (Platform.OS === 'ios') {
+        getOneTimeLocation();
+        subscribeLocationLocation();
+      } else {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            {
+              title: 'Location Access Required',
+              message: 'This App needs to Access your location',
+            },
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            //To Check, If Permission is granted
+            getOneTimeLocation();
+            subscribeLocationLocation();
+          } else {
+            setLocationStatus('Permission Denied');
+          }
+        } catch (err) {
+          console.warn(err);
+        }
+      }
+    };
+    requestLocationPermission();
+    return () => {
+      Geolocation.clearWatch(watchID);
+    };
+  }, []);
+
+  const getOneTimeLocation = () => {
+    setLocationStatus('Getting Location ...');
+    Geolocation.getCurrentPosition(
+      //Will give you the current location
+      position => {
+        setLocationStatus('You are Here');
+
+        //getting the Longitude from the location json
+        const currentLongitude = JSON.stringify(position.coords.longitude);
+
+        //getting the Latitude from the location json
+        const currentLatitude = JSON.stringify(position.coords.latitude);
+
+        //Setting Longitude state
+        setCurrentLongitude(currentLongitude);
+
+        //Setting Longitude state
+        setCurrentLatitude(currentLatitude);
+      },
+      error => {
+        setLocationStatus(error.message);
+      },
+      {
+        enableHighAccuracy: false,
+        timeout: 30000,
+        maximumAge: 1000,
+      },
+    );
+  };
+
+  const subscribeLocationLocation = () => {
+    watchID = Geolocation.watchPosition(
+      position => {
+        //Will give you the location on location change
+
+        setLocationStatus('You are Here');
+        console.log(position);
+
+        //getting the Longitude from the location json
+        const currentLongitude = JSON.parse(position.coords.longitude);
+
+        //getting the Latitude from the location json
+        const currentLatitude = JSON.parse(position.coords.latitude);
+
+        //Setting Longitude state
+        setCurrentLongitude(currentLongitude);
+
+        //Setting Latitude state
+        setCurrentLatitude(currentLatitude);
+      },
+      error => {
+        setLocationStatus(error.message);
+      },
+      {
+        enableHighAccuracy: false,
+        maximumAge: 1000,
+      },
+    );
   };
 
   return (
@@ -29,69 +155,12 @@ const UsersMapScreen = ({navigation}) => {
           <Marker
             draggable
             coordinate={{
-              latitude: 44.8059307,
-              longitude: -0.5996017,
+              latitude: currentLatitude,
+              longitude: currentLongitude,
             }}
             onDragEnd={e => alert(JSON.stringify(e.nativeEvent.coordinate))}
             title={'Vous êtes ici'}
             //description={'This is a description of the marker'}
-          />
-          <Marker
-            pinColor={'#772372'}
-            draggable
-            coordinate={{
-              latitude: 44.83476,
-              longitude: -0.573644,
-            }}
-            onDragEnd={e => alert(JSON.stringify(e.nativeEvent.coordinate))}
-            title={'Marie Apparailly'}
-            description={'0626221915'}
-          />
-          <Marker
-            pinColor={'#242333'}
-            draggable
-            coordinate={{
-              latitude: 44.82476,
-              longitude: -0.583644,
-            }}
-            onDragEnd={e => alert(JSON.stringify(e.nativeEvent.coordinate))}
-            title={'Tiphaine Petit'}
-            description={'0745887110'}
-          />
-          <Marker
-            image={{
-              uri: 'https://www.pngall.com/wp-content/uploads/2017/05/Map-Marker-PNG-Image.png',
-            }}
-            draggable
-            coordinate={{
-              latitude: 44.83076,
-              longitude: -0.595644,
-            }}
-            onDragEnd={e => alert(JSON.stringify(e.nativeEvent.coordinate))}
-            title={'Joseph Beasse'}
-            description={'0624785022'}
-          />
-          <Marker
-            pinColor={'#347139'}
-            draggable
-            coordinate={{
-              latitude: 44.82876,
-              longitude: -0.585644,
-            }}
-            onDragEnd={e => alert(JSON.stringify(e.nativeEvent.coordinate))}
-            title={'Mathys Traversier'}
-            description={'0784556120'}
-          />
-          <Marker
-            pinColor={'#746855'}
-            draggable
-            coordinate={{
-              latitude: 44.80476,
-              longitude: -0.583644,
-            }}
-            onDragEnd={e => alert(JSON.stringify(e.nativeEvent.coordinate))}
-            title={'Clémence Monnier'}
-            description={'0678900043'}
           />
         </MapView>
       </View>
@@ -204,3 +273,125 @@ const styles = StyleSheet.create({
     left: 30,
   },
 });
+
+// const mapStyle = [
+//   {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+//   {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+//   {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+//   {
+//     featureType: 'administrative.locality',
+//     elementType: 'labels.text.fill',
+//     stylers: [{color: '#d59563'}],
+//   },
+//   {
+//     featureType: 'poi',
+//     elementType: 'labels.text.fill',
+//     stylers: [{color: '#d59563'}],
+//   },
+//   {
+//     featureType: 'poi.park',
+//     elementType: 'geometry',
+//     stylers: [{color: '#263c3f'}],
+//   },
+//   {
+//     featureType: 'poi.park',
+//     elementType: 'labels.text.fill',
+//     stylers: [{color: '#6b9a76'}],
+//   },
+//   {
+//     featureType: 'road',
+//     elementType: 'geometry',
+//     stylers: [{color: '#38414e'}],
+//   },
+//   {
+//     featureType: 'road',
+//     elementType: 'geometry.stroke',
+//     stylers: [{color: '#212a37'}],
+//   },
+//   {
+//     featureType: 'road',
+//     elementType: 'labels.text.fill',
+//     stylers: [{color: '#9ca5b3'}],
+//   },
+//   {
+//     featureType: 'road.highway',
+//     elementType: 'geometry',
+//     stylers: [{color: '#746855'}],
+//   },
+//   {
+//     featureType: 'road.highway',
+//     elementType: 'geometry.stroke',
+//     stylers: [{color: '#1f2835'}],
+//   },
+//   {
+//     featureType: 'road.highway',
+//     elementType: 'labels.text.fill',
+//     stylers: [{color: '#f3d19c'}],
+//   },
+//   {
+//     featureType: 'transit',
+//     elementType: 'geometry',
+//     stylers: [{color: '#2f3948'}],
+//   },
+//   {
+//     featureType: 'transit.station',
+//     elementType: 'labels.text.fill',
+//     stylers: [{color: '#d59563'}],
+//   },
+//   {
+//     featureType: 'water',
+//     elementType: 'geometry',
+//     stylers: [{color: '#17263c'}],
+//   },
+//   {
+//     featureType: 'water',
+//     elementType: 'labels.text.fill',
+//     stylers: [{color: '#515c6d'}],
+//   },
+//   {
+//     featureType: 'water',
+//     elementType: 'labels.text.stroke',
+//     stylers: [{color: '#17263c'}],
+//   },
+// ];
+
+// const styles = StyleSheet.create({
+//   container: {
+//     position: 'absolute',
+//     top: 0,
+//     left: 0,
+//     right: 0,
+//     bottom: 0,
+//     alignItems: 'center',
+//     justifyContent: 'flex-end',
+//   },
+//   mapStyle: {
+//     position: 'absolute',
+//     top: 0,
+//     left: 0,
+//     right: 0,
+//     bottom: 0,
+//   },
+//   switchList: {
+//     flexDirection: 'row-reverse',
+//     position: 'absolute',
+//     top: 100,
+//     left: 30,
+//   },
+// });
+
+// // const styles = StyleSheet.create({
+// //   container: {
+// //     flex: 1,
+// //     backgroundColor: 'white',
+// //     padding: 10,
+// //     alignItems: 'center',
+// //     justifyContent: 'center',
+// //   },
+// //   boldText: {
+// //     fontSize: 25,
+// //     color: 'red',
+// //     marginVertical: 16,
+// //     textAlign: 'center',
+// //   },
+// // });
